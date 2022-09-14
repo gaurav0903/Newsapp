c74b383b62ec90190ed2912c6846ab42a85ef54b
gaurav0903 <saxena090320expert@gmail.com>
2022-09-14 11:54:24 +0530

    navbar change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -40,32 +40,32 @@ export default class Navbar extends Component {
   render() {
 
     return (
-        // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        // <Link className="navbar-brand" to="/">News Point</Link>
-        // <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //   <span className="navbar-toggler-icon"></span>
-        // </button>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">News Point</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
       
-        // <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //   <ul className="navbar-nav mr-auto">
-        //     <li className="nav-item active">
-        //       <Link className="nav-link" to="/">Home </Link>
-        //     </li>
-        //     <li className="nav-item">
-        //       <Link className="nav-link" to="/business">Business</Link>          
-        //     </li>
-        //        <li>
-        //         <Link className="nav-link" to="/entertainment">Entertainment</Link>
-        //       </li>
-        //       <li><Link className="nav-link" to="/health">Health</Link>
-        //      </li>
-        //       <li> <Link className="nav-link" to="/science">Science</Link>
-        //       </li>
-        //       <li><Link className="nav-link" to="/sports">Sports</Link>
-        //       </li>
-        //       <li><Link className="nav-link" to="/technology">Technology</Link></li>         
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home </Link>
+           </li>
+           <li className="nav-item">
+             <Link className="nav-link" to="/business">Business</Link>          
+           </li>
+              <li>
+               <Link className="nav-link" to="/entertainment">Entertainment</Link>
+             </li>
+             <li><Link className="nav-link" to="/health">Health</Link>
+            </li>
+             <li> <Link className="nav-link" to="/science">Science</Link>
+             </li>
+             <li><Link className="nav-link" to="/sports">Sports</Link>
+             </li>
+             <li><Link className="nav-link" to="/technology">Technology</Link></li>         
 
-           /* <li className="nav-item">
+           <li className="nav-item">
               <Link className="nav-link" to="/">Link</Link>
             </li>
             <li className="nav-item dropdown">
@@ -78,43 +78,43 @@ export default class Navbar extends Component {
                 <div className="dropdown-divider"></div>
                 <Link className="dropdown-item" to="/">Something else here</Link>
               </div>
-            </li> */ 
-             /* <li className="nav-item">
+            </li> 
+           <li className="nav-item">
                <Link className="nav-link disabled" to="/">Disabled</Link>
-             </li> */
-          //  </ul>
-           /* <form className="form-inline my-2 my-lg-0">
+             </li>
+           </ul>
+           <form className="form-inline my-2 my-lg-0">
              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
              <button className="btn btn-outline-success my-2 my-sm-0"type="submit">Search</button>
-           </form> */
-      //    </div>
-      // </nav>
+           </form> 
+          </div>
+       </nav>
       
-      <nav className="nav">
-     <ul className="list">
-      <li className='item'>
-     <Link className='news' to="/"><b>News Point</b></Link>
-     </li>
-          <li className="item">
-            <Link className="link"to="/"><b>Home</b> </Link>
-          </li>
-          <li className='item'>
-            <Link  className="link"  to="/business">Business</Link>
-          </li>
-             <li className="item">
-              <Link  className="link" to="/entertainment">Entertainment</Link>
-            </li >
-            <li className="item"><Link className="link" to="/health">Health</Link>
-           </li>
-            <li className="item"> <Link className="link" to="/science">Science</Link>
-            </li>
-            <li className="item"><Link className="link" to="/sports">Sports</Link>
-            </li>
-            <li className="item"><Link className="link" to="/technology">Technology</Link></li>
-        </ul>
+    //   <nav className="nav">
+    //  <ul className="list">
+    //   <li className='item'>
+    //  <Link className='news' to="/"><b>News Point</b></Link>
+    //  </li>
+    //       <li className="item">
+    //         <Link className="link"to="/"><b>Home</b> </Link>
+    //       </li>
+    //       <li className='item'>
+    //         <Link  className="link"  to="/business">Business</Link>
+    //       </li>
+    //          <li className="item">
+    //           <Link  className="link" to="/entertainment">Entertainment</Link>
+    //         </li >
+    //         <li className="item"><Link className="link" to="/health">Health</Link>
+    //        </li>
+    //         <li className="item"> <Link className="link" to="/science">Science</Link>
+    //         </li>
+    //         <li className="item"><Link className="link" to="/sports">Sports</Link>
+    //         </li>
+    //         <li className="item"><Link className="link" to="/technology">Technology</Link></li>
+    //     </ul>
       
       
-     </nav>
+    //  </nav>
    
               
     )
